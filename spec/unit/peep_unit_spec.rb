9dcf332a4b264b6f25fa7703e947ea32cd59d197
David Millen <70208036+dm-devtech@users.noreply.github.com>
2021-07-12 17:55:42 +0100

    test: corrected all unit tests

--- spec/unit/peep_unit_spec.rb
+++ spec/unit/peep_unit_spec.rb
@@ -5,31 +5,34 @@ describe Peep do
       it 'returns all peeps' do
         connection = PG.connect(dbname: 'chitter_test')
         connection.query('TRUNCATE all_peeps RESTART IDENTITY CASCADE;')
-        test = connection.query("SELECT * FROM all_peeps;")
-        puts test
 
-        peepone = Peep.create(peep_text: 'My first test peep', time: 'NOW')
+        peepone = Peep.create(peep_text: 'My first test peep', time: 'NOW()')
         peeptwo = Peep.create(peep_text: 'My second peep', time: 'NOW()')
 
         peeps = Peep.all
 
         expect(peeps.length).to eq 2
         expect(peeps.first).to be_a Peep
-        expect(peeps.first.id).to eq peepone.id
-        expect(peeps.first.peep_text).to eq 'My first peep'
-        expect(peeps.first.time).to eq(timenow[0...16])
+        expect(peeps[0].id).to eq "2"
+        expect(peeps[0].peep_text).to eq 'My second peep'
+        expect(peeps[0].time[0...16]).to eq(Time.now.strftime("%Y-%m-%d %H:%M"))
+
+        expect(peeps[1]).to be_a Peep
+        expect(peeps[1].id).to eq "1"
+        expect(peeps[1].peep_text).to eq 'My first test peep'
+        expect(peeps[1].time[0...16]).to eq(Time.now.strftime("%Y-%m-%d %H:%M"))
       end
     end
 
-    # describe '#create' do
-    #   it 'creates a new peep' do
-    #     data = PG.connect(dbname: 'chitter_test').query("SELECT * FROM all_peeps WHERE id = #{peep.id};")
-    #     peep = Peep.create(peep_text: 'My second test peep', time: 'NOW()')
-    #     timenow = Time.now.to_s
-    #     expect(peep).to be_a Peep
-    #     expect(peep.id).to eq data.first['id']
-    #     expect(peep.peep_text).to eq 'My second test peep'
-    #     expect(peep.time[0...16]).to eq(timenow[0...16])
-    #   end
-    # end
+    describe '#create' do
+      it 'creates a new peep' do
+        peep = Peep.create(peep_text: 'My second test peep', time: 'NOW()')
+        data = PG.connect(dbname: 'chitter_test').query("SELECT * FROM all_peeps WHERE id = #{peep.id};")
+        timenow = Time.now.to_s
+        expect(peep).to be_a Peep
+        expect(peep.id).to eq data.first['id']
+        expect(peep.peep_text).to eq 'My second test peep'
+        expect(peep.time[0...16]).to eq(timenow[0...16])
+      end
+    end
 end
